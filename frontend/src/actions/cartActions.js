1383e8861c23de7176e99b104f154e4d3ac1d2fb
Rayson Dsouza <rayson789foru@gmail.com>
2022-04-15 14:29:50 +0530

    added cart item data into cart component

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { json } from "express";
+
 
 //thunk is an func within a func
 export const addToCart = (id) => async(dispatch,getState)=>{
--- frontend/src/screens/Cart.jsx
+++ frontend/src/screens/Cart.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
-
+import {useSelector,useDispatch} from'react-redux'
+import {useEffect} from 'react'
+import { useParams, Link} from 'react-router-dom'
+import {addToCart} from '../actions/cartActions'
 function Cart() {
+  const param = useParams()
+  const dispatch = useDispatch()
+
+  const cartList = useSelector(state => state.cart)
+  const {cartItems} = cartList
+  useEffect(() => {
+    dispatch(addToCart(param.id))
+  }, [dispatch,param])
   return (
     <h1>Cart</h1>
   )
