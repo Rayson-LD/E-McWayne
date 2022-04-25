652298a868f8a464cebed993aef72cd04ae1e210
Rayson Dsouza <rayson789foru@gmail.com>
2022-04-25 18:07:12 +0530

    @backend product reeview added

--- backend/routes/ProdctRoutes.js
+++ backend/routes/ProdctRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Product from "../models/productModel.js";
+import { protect } from "../middleware/authMiddleware.js";
 const Productrouter = express.Router()
 //@desc - router for getting products from mongoose product model as all products or single product
 Productrouter.get('/',async(req,res)=>{
@@ -26,5 +27,37 @@ Productrouter.get('/:id',async(req,res)=>{
         res.status(400).json({message:"Product not found"})
     }
 
+})
+Productrouter.post('/:id/reviews',protect,async(req,res)=>{
+
+    try {
+        const {rating,comment} = req.body
+        const p = await Product.findById(req.params.id);
+        if(p)
+        {
+            const reviewed = await p.reviews.find(r => r.user.toString() === req.user.id.toString())
+            if(reviewed)
+            {
+                res.status(400).json('Product Already Reviewed')
+            }
+     
+                const review = {
+                    name : req.user.name,
+                    rating:Number(rating),
+                    comment,
+                    user:req.user.id
+                }
+               
+                p.reviews.push(review)
+                p.numReviews = p.reviews.length
+                p.rating = p.reviews.reduce((acc,item)=> item.rating+acc,0)/p.numReviews
+                await p.save()
+                res.status(201).json('Review Added')
+            
+        }
+    } catch (error) {
+        console.log(error)
+    }
+  
 })
 export default Productrouter
