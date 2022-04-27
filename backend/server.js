045f859eb42a2efd5b843a9346ceee72f04968c2
Rayson Dsouza <rayson789foru@gmail.com>
2022-04-27 19:17:30 +0530

    header cart details added

--- backend/server.js
+++ backend/server.js
@@ -14,5 +14,12 @@ app.use('/api/users',userRouter)
 app.use('/api/orders',orderRoutes)
 
 app.get('/api/config/paypal',(req,res)=>res.send(process.env.PAYPAL_CLIENT_ID))
+
+if(process.env.mode === 'production')
+{
+    const __dirname = path.resolve()
+    app.use(express.static(path.join(__dirname,'/frontend/build')))
+    app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
+}
 const port = process.env.PORT || 5000;
 app.listen(port,console.log(`Server Running on ${port} in ${process.env.mode} mode`));
